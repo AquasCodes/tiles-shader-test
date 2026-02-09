cd5df74da5cddbaae61db3d13283973f3df444a2
Daniel <danielrportillo@gmail.com>
2026-02-09 02:33:37 -0600

    added SOOOO much stuff bro...

---
+++ collectible.gd
@@ -0,0 +1,40 @@
+@tool
+extends Area2D
+class_name Collectible
+
+@export var anim_speed: float = 1
+@export var bob_magnitude: float = 3
+
+var is_collected: bool = false
+var anim_timer: float
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var shine_particles: CPUParticles2D = $ShineParticles
+@onready var collect_particles: CPUParticles2D = $CollectParticles
+@onready var collect_noise: AudioStreamPlayer = $CollectNoise
+
+func _ready() -> void:
+	shine_particles.emitting = false
+	anim_timer = randf_range(0, 100)
+	sprite.frame = int(hash(hash(position.x) + hash(position.y))) % sprite.hframes
+	
+	await get_tree().create_timer(randf_range(0, shine_particles.lifetime), false).timeout
+	
+	shine_particles.emitting = true
+
+func _process(delta: float) -> void:
+	if not is_collected:
+		anim_timer += delta
+		sprite.position.y = sin(anim_timer * anim_speed) * bob_magnitude
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player and not is_collected:
+		sprite.visible = false
+		shine_particles.visible = false
+		collect_particles.emitting = true
+		collect_noise.play()
+		is_collected = true
+		
+		await collect_particles.finished
+		
+		queue_free()
--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 enum Directions {LEFT = -1, RIGHT = 1}
 
@@ -8,20 +9,44 @@ enum Directions {LEFT = -1, RIGHT = 1}
 
 @export var jump_vel: float
 
+@export var camera_lead_px: float
+
+var direction: Directions = Directions.RIGHT
+var prev_velocity: Vector2
+
+@onready var camera_2d: Camera2D = $Camera2D
+
+func _ready() -> void:
+	prev_velocity = velocity
+	camera_2d.position.x = direction * camera_lead_px
+	await get_tree().process_frame
+	$Camera2D.reset_smoothing()
+
 func _physics_process(delta: float) -> void:
+	if is_on_ceiling():
+		pass # Bonk head
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	var walk_input: float = get_walk_input()
 	if walk_input:
 		velocity.x = move_toward(velocity.x, max_speed * walk_input, delta * accel)
+		
+		direction = Directions.RIGHT if walk_input > 0 else Directions.LEFT
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, delta * decel)
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_vel
 	
+	prev_velocity = velocity
 	move_and_slide()
+	
+	camera_2d.position.x = move_toward(camera_2d.position.x, direction * camera_lead_px, delta * camera_lead_px * 5.0)
+
+func _process(_delta: float) -> void:
+	$Icon.scale.x = direction
 
 func get_walk_input() -> float:
 	return sign(Input.get_axis("walk_left", "walk_right"))
